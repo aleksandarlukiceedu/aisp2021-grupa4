7d3ca220b2d93557ac282e0b9a4ce8a52282a5ed
Aleksandar Lukić <93lukic@live.com>
2021-03-04 09:14:50 +0100

    Izbacen ispis

--- vezba1/src/mojpaket/Program.java
+++ vezba1/src/mojpaket/Program.java
@@ -3,7 +3,7 @@ package mojpaket;
 public class Program {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int a = 5;
         System.out.println("Hello World!");
     }
 }
